d314c2c07470f2b616244fb15d7ca54b8606733b
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-01-17 21:55:29 -0300

    configuração finalizada e inicio a tela login

--- app/src/main/java/com/senai/vsconnect_kotlin/MainActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/MainActivity.kt
@@ -29,10 +29,6 @@ class MainActivity : AppCompatActivity() {
         //desabilita a exibiçao do titulo do nome da tela atual
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
