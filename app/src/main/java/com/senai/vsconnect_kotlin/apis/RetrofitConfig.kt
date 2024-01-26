f18895682230262ef4969a54effdbf2c747bdefd
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-01-25 21:57:04 -0300

    configigurado a perimissao de rede nao segura/adapter criado/consumo de api na lista de servicos feita

--- app/src/main/java/com/senai/vsconnect_kotlin/apis/RetrofitConfig.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/apis/RetrofitConfig.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitConfig {
     companion object{
-        fun obterInstanciaRetrofit(url: String = "http://172.16.52.193:8099/") : Retrofit{
+        fun obterInstanciaRetrofit(url: String = "http://172.16.27.219:8099/") : Retrofit{
             return Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
