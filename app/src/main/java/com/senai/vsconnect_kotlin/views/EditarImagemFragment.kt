6fb5ebae262606aded72cf6692c997fd752fb5df
unknown <Alexia@DESKTOP-AP18MAI.info.local>
2024-01-30 19:30:46 -0300

    continuação chamada da api buscar usuario por ID

--- app/src/main/java/com/senai/vsconnect_kotlin/views/EditarImagemFragment.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/views/EditarImagemFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.gson.JsonObject
 import com.senai.vsconnect_kotlin.apis.EndpointInterface
 import com.senai.vsconnect_kotlin.apis.RetrofitConfig
 import com.senai.vsconnect_kotlin.databinding.FragmentEditarImagemBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 class EditarImagemFragment : Fragment() {
 
@@ -30,7 +35,18 @@ class EditarImagemFragment : Fragment() {
         return root
     }
 
+    private fun buscarUsuarioPorID(idUsuario: String) {
+        endpoints.buscarUsuarioPorID(UUID.fromString(idUsuario)).enqueue(object : Callback<JsonObject>{
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 
 
     override fun onDestroyView() {
